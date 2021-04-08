516a433ce77761d588c90de5bb906b964c159078
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-04-08 21:45:12 +0300

    bug fixes

--- app/src/main/java/com/example/yaroslavgorbach/randomizer/number/NumberUtils.kt
+++ app/src/main/java/com/example/yaroslavgorbach/randomizer/number/NumberUtils.kt
@@ -1,18 +1,12 @@
 package com.example.yaroslavgorbach.randomizer.number
 
 import android.content.Context
-import android.graphics.Point
-import android.view.Display
-import android.view.WindowManager
-
 
 class NumberUtils {
     companion object{
          fun getScreenHeight(context: Context): Int {
-             val display = context.display
-             val size = Point()
-              display?.getRealSize(size)
-             return size.x
+             val display  = context.resources.displayMetrics
+             return display.heightPixels
          }
     }
 }
